3f1f88c3cafba3de2488672c39fe4aba2732e535
K-s-c49 <khushalsinghchundawat144@gmail.com>
2025-06-17 04:15:07 -0700

    add setting for live web

--- ec/settings.py
+++ ec/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7qt%e$1av67x0zmr@o00c%*5zz6p^v6=b8et10_w7$t396w4@l'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-7qt%e$1av67x0zmr@o00c%*5zz6p^v6=b8et10_w7$t396w4@l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['e-commerce-xb95.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 #'e-commerce-xb95.onrender.com'
 
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/"media"
 
